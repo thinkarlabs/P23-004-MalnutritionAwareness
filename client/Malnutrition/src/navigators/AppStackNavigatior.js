339098ba7350f65d5fde7b238e27fb9f3c205f17
deepak-quovantis <deepak.prasad@quovantis.com>
2023-02-03 14:10:46 +0530

    MAL-2 | added routing and component

--- client/Malnutrition/src/navigators/AppStackNavigatior.js
+++ client/Malnutrition/src/navigators/AppStackNavigatior.js
@@ -1,27 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Onboarding from "../modules/onboarding";
-import Splash from "../modules/splash";
+import React, {useState, useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Onboarding from '../modules/onboarding';
+import Splash from '../modules/splash';
+import CreateAccount from '../modules/create-account';
 
 const Stack = createStackNavigator();
 
 export function AppStackNavigator() {
-    const [showSplashScreen, setShowSplashScreen] = useState(true);
-    useEffect(() => {
-        setTimeout(() => {
-            setShowSplashScreen(false);
-        },3000)
-    },[])
-    return (
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false, }}>
-                {
-                    showSplashScreen ? <Stack.Screen name="Splash" component={Splash} />
-                        : null
-                }
-                <Stack.Screen name="Onboarding" component={Onboarding} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+  const [showSplashScreen, setShowSplashScreen] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setShowSplashScreen(false);
+    }, 3000);
+  }, []);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        {showSplashScreen ? (
+          <Stack.Screen name="Splash" component={Splash} />
+        ) : null}
+        <Stack.Screen name="Onboarding" component={Onboarding} />
+        <Stack.Screen name="CreateAccount" component={CreateAccount} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
