2f603dc9b462b8ce5aa06f726b11c8976240572b
sadam-123 <103425932+sadam-123@users.noreply.github.com>
2023-02-06 19:57:36 +0530

    Update user.py

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -4,6 +4,7 @@ from fastapi.encoders import jsonable_encoder
 from config.database import db as database
 from schemas.user import serializeDict, serializeList
 from bson import ObjectId
+import random
 
 router = APIRouter()
 
@@ -45,6 +46,12 @@ async def create_account(user: UserSchema= Body(...)):
     new_user = database.user_collection.insert_one(user_dict)
 
     return {'status_code':200, 'message': 'User saved successfully'}
+    # generate and save otp for user
+    def otp_generate_save(phone_number, db):
+        otp = str(random.randint(1000, 9999))
+
+        db.otp_map.update_one({"phone_number": phone_number}, {"$set": {"otp": otp}}, upsert=True)
+
 
 
 #
