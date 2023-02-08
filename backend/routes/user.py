ec4757a4de19e2301ad7aea0d0801d20818ed47d
sadam-123 <103425932+sadam-123@users.noreply.github.com>
2023-02-08 13:25:26 +0530

    generate and send otp for user function added

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -44,14 +44,17 @@ async def create_account(user: UserSchema= Body(...)):
         raise HTTPException(status_code=409, detail="Phone number already exists !")
 
     new_user = database.user_collection.insert_one(user_dict)
-
-    return {'status_code':200, 'message': 'User saved successfully'}
     # generate and save otp for user
     def otp_generate_save(phone_number, db):
         otp = str(random.randint(1000, 9999))
 
         db.otp_map.update_one({"phone_number": phone_number}, {"$set": {"otp": otp}}, upsert=True)
+    # send otp to users phone number
+    def send_otp_to_phone(phone_number, otp):
+        print(f"Sending OTP {otp} to phone number {phone_number}")
 
+    return {'status_code':200, 'message': 'User saved successfully'}
+    
 
 
 #
