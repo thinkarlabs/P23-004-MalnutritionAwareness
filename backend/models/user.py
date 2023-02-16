587bfc61981d687cb05eb19bc3c1d098e3f9bfc3
Sadam Ramya <SadamR@emids.com>
2023-02-16 08:59:24 +0530

    Update user model and routes

--- backend/models/user.py
+++ backend/models/user.py
@@ -3,25 +3,36 @@ import datetime
 from typing import Optional, List
 from pydantic import BaseModel, validator, constr
 
+
+class UserRole(str, Enum):
+    ANGANVADI = 'ANGANVADI'
+    NGO_MEMBER = 'NGO_MEMBER'
+    RELATIVE = 'RELATIVE'
+
+
 class UserType(str, Enum):
     PREGNANT = 'PREGNANT'
     LACTATING = 'LACTATING'
     CAREGIVER = 'CAREGIVER'
 
+
 class GenderType(str, Enum):
     MALE = 'MALE'
     FEMALE = 'FEMALE'
+
 
 class Child(BaseModel):
     name: str
     gender: GenderType
     dob: datetime.date
+
     @validator("dob", pre=True)
     def parse_dob(cls, value):
         return datetime.datetime.strptime(
             value,
             "%d/%m/%Y"
         ).date()
+
 
 class CreateUserSchema(BaseModel):
     name: str
@@ -30,9 +41,11 @@ class CreateUserSchema(BaseModel):
         regex=r"^(\+)[1-9][0-9\-\(\)\.]{9,15}$",
     )
     user_type: UserType
+    user_role: UserRole
     child: List[Child]
     relation_with_child: Optional[str]
     is_created_for_someone_else: Optional[bool] = False
+
 
 def ResponseModel(data, message):
     return {
--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Body, HTTPException
-from models.user import CreateUserSchema, ResponseModel
+from models.user import CreateUserSchema, ResponseModel, UserRole
 from fastapi.encoders import jsonable_encoder
 from config.database import db as database
 from schemas.user import serializeDict, serializeList
@@ -25,20 +25,23 @@ def otp_generate_save(phone_number):
 def send_otp_to_phone(phone_number, otp):
     print("Sent OTP to phone successfully !!")
 
+
 @router.post("/account")
-async def create_account(user: CreateUserSchema= Body(...)):
+async def create_account(user: CreateUserSchema = Body(...)):
     user_dict = jsonable_encoder(user)
 
     user_dict.update({'is_active': False})
+
     # Check if the user wants to create an account for someone else
-    if user.create_for_someone_else:
+    if user.is_created_for_someone_else:
         # Get the name of the person for whom the account is being created
-        full_name = user.full_name
+        full_name = user.name
 
         # Add the new user to the database with the specified information
         user_dict.update({
             'full_name': full_name
         })
+
     else:
         # check if same record exists
         if database.user.find_one({'phone_number': user_dict['phone_number']}):
@@ -51,6 +54,7 @@ async def create_account(user: CreateUserSchema= Body(...)):
     # # send otp to users phone number
     send_otp_to_phone(user_dict['phone_number'], otp)
 
-    return {'status_code':200, 'message': 'User saved successfully'}
+    return {'status_code': 200, 'message': 'User saved successfully'}
+
 
     
