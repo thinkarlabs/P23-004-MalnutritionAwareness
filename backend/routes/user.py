b21e1888b1fb35976d6a1a518496720551f4ce50
Bhavesh Holey <BhaveshH@emids.com>
2023-02-22 17:40:37 +0530

    OTP verification and expiration

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -9,18 +9,20 @@ from bson import ObjectId
 import random
 from fastapi.security import OAuth2PasswordBearer
 import jwt
+import datetime
 
 
 router = APIRouter()
 
 def otp_generate_save(phone_number):
-    # Generate 4 digit random OTP
+    # Generate 4 digit random OTP and timestamp
     otp = random.randint(1000, 9999)
+    timestamp = datetime.datetime.now()
 
     # Upsert the phone_number:otp mapping
     database.otp_mapping.update_one(
         {'phone_number': phone_number},
-        {"$set": {'otp': otp}},
+        {"$set": {'otp': otp, 'timestamp': timestamp}},
         upsert=True
     )
 
@@ -54,10 +56,16 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/verify_otp")
 
 @router.post("/api/v1/verify_otp")
 async def verify_otp(vphone_number: str, votp: int, is_creation: bool):
+    # check expiration of entered OTP
+    curr_time = datetime.datetime.now()
+    otp_gen_time = database.otp_mapping.find_one({'phone_number': vphone_number, 'otp': votp},{'timestamp': 1,'_id': 0})
+    valid_time = otp_gen_time['timestamp'] + datetime.timedelta(seconds=60)
+    is_otp_valid = curr_time <= valid_time
+
     # Compare the provided OTP with the stored OTP
-    if database.otp_mapping.find_one({'phone_number': vphone_number, 'otp': votp}):
+    if database.otp_mapping.find_one({'phone_number': vphone_number, 'otp': votp}) and is_otp_valid:
         session_token = jwt.encode({'phone_number': vphone_number}, JWT_SECRET, algorithm=JWT_ALGORITHM)
-        response = VerifyOTPResponse(message="OTP verified successfully", session_token=session_token)
+        response = VerifyOTPResponse(message= "OTP verified successfully", session_token=session_token)
         if is_creation:
             database.user.update_one(
                 {'phone_number': vphone_number},
